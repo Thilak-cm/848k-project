d953cf5ed5bde722123860b874eebb3585f4f843
Peeyush4 <peeyu704@gmail.com>
2024-11-08 19:19:44 -0500

    Torchrun and Dataloader tested code

--- dataset/finewebDataset.py
+++ dataset/finewebDataset.py
@@ -1,39 +1,57 @@
 import requests
 import torch
-from transformers import AutoTokenizer
+import tiktoken
+import numpy as np
+import os
 
+def load_tokens(filename):
+    try: npt = np.load(filename, allow_pickle=True)
+    except: npt = np.fromfile(filename, dtype=np.uint16)  # Replace dtype as needed
+
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt 
 
 
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_processes, device='cpu'):
+    def __init__(self, B, T, process_rank, num_processes,split, device ='cpu',):
         self.B, self.T = B, T
         self.device = device
         self.process_rank = process_rank
         self.num_processes = num_processes
-        information = requests.get("https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt")
-        text = information.text
-        # If you use this directly => tokens = tokenizer.encode(text, return_tensors='pt')
-        # You'll get a warning because the text is too long and the model is too small because the model can take only 1024 tokens
-        tokenizer = AutoTokenizer.from_pretrained('gpt2')
-        self.tokens = tokenizer.encode(text, return_tensors='pt')
-        print(f"Loaded {len(self.tokens[0])} tokens")
-        print(f"1 epoch = {len(self.tokens[0]) // (self.B * self.T)} iterations")
+        assert split in {'train', 'val'}
 
-        # State 
-        # Process 0 will start from 0, Process 1 will start from B*T, Process 2 will start from 2*B*T, etc.
+        master_process = process_rank ==0
+        #get the shared filenames
+        CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+        data_root = os.path.join(CURRENT_DIR, "edu_fineweb10B")
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        
+        #state, init and shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
+
 
     def next_batch(self):
         B, T = self.B, self.T
-        buf = torch.tensor(self.tokens[0][self.current_position:self.current_position + B*T + 1])
-        x = buf[:-1].view(B, T).to(self.device) 
-        y = buf[1:].view(B, T).to(self.device)
+        buf = torch.tensor(self.tokens[self.current_position:self.current_position + B*T + 1])
+        x = buf[:-1].view(B, T).to(self.device) #inputs
+        y = buf[1:].view(B, T).to(self.device)  #targets
         
-        # We need to jump B*T*num_processes to get the next batch
+        # We need to advance position B*T*num_processes to get the next batch in tensor
         self.current_position += B*T*self.num_processes
 
-        # If we reach the end of the dataset, we need to start from the beginning
-        if self.current_position + B*T*self.num_processes + 1 >= len(self.tokens):
-            self.current_position = self.B * self.T * self.process_rank
-        return x, y
+        # If loading the next shard would be out of bounds, advance to the next shard
+        if self.current_position +(B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+        return x,y
 
