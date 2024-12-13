7a583317335a3cdd1e53660f266dd02855f584f7
Peeyush Dyavarashetty <c848k017@nexusclass00.umiacs.umd.edu>
2024-12-13 16:58:12 -0500

    Added linformer codes, hellaswag for linformer, changed upload pth file code and submit.sh for direct pushing to wandb

--- upload_pth_files.py
+++ upload_pth_files.py
@@ -1,12 +1,29 @@
 import wandb
+import os
+import re
 
 wandb.init(project="GPT 2 848K Nexus Cluster")
 
 # Create an artifact
 artifact = wandb.Artifact("final-models", type="model")
 
-# Add files to the artifact
-artifact.add_file("/fs/nexus-scratch/thilakcm/FIRE/final_epoch_model.pth")
+# Find the path for the model folders 
+path = os.path.dirname(os.path.abspath(__file__))
+if path != '/fs/nexus-scratch/thilakcm/848k-project':
+    pattern = r'c848k\d+'
+    account = re.findall(pattern, path)[0]
+    save_folder = f'/fs/class-projects/fall2024/cmsc848k/{account}'
+    os.makedirs(save_folder, exist_ok=True)
+else:
+    save_folder = '/fs/nexus-scratch/thilakcm'
+    os.makedirs(save_folder, exist_ok=True)
+
+# Add files to the artifact from all the folders
+for folder in os.listdir(save_folder):
+    model_path = f"{save_folder}/{folder}/final_epoch_model.pth"
+    if os.path.exists(model_path):
+        print(f"{model_path} exists")
+        artifact.add_file(model_path)
 
 # Save the artifact
 wandb.log_artifact(artifact)
