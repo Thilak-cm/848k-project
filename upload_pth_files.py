4df031690e492a6f0017ab0f75b03f5e68937835
Peeyush Dyavarashetty <c848k017@nexusclass00.umiacs.umd.edu>
2024-12-13 17:19:44 -0500

    Changed upload_pth_files to upload files in specified name

--- upload_pth_files.py
+++ upload_pth_files.py
@@ -23,7 +23,7 @@ for folder in os.listdir(save_folder):
     model_path = f"{save_folder}/{folder}/final_epoch_model.pth"
     if os.path.exists(model_path):
         print(f"{model_path} exists")
-        artifact.add_file(model_path)
+        artifact.add_file(local_path=model_path, name=f"{folder}_model.pth")
 
 # Save the artifact
 wandb.log_artifact(artifact)
